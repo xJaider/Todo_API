7fd5ec95d1b6a9a2ce969590bd21d9c631f41e02
Jaider <sanchezj0294@gmail.com>
2025-04-13 20:55:29 -0500

    Configuracion para la base de datos.

---
+++ src/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine # para crear el motor de SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+dialecto = os.getenv("DB_CONNECTION")
+username = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+database = os.getenv("DB_NAME")
+
+database_url = f"{dialecto}://{username}:{password}@{host}:{port}/{database}"
+
+engine = create_engine(database_url, echo=True) # Creando el motor de conexion
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base() # Creando la clase Base para que los modelos puedan heredarlas
---
+++ src/models.py
@@ -0,0 +1,33 @@
+import enum
+
+from sqlalchemy import Column, Integer, String, Enum, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime, timezone
+from .database import Base
+
+class RolUsuario(enum.Enum):
+    ADMIN = "admin"
+    USUARIO = "usuario"
+
+class EstadoTarea(enum.Enum):
+    PENDIENTE = "pendiente"
+    COMPLETADO = "completado"
+
+class Usuario(Base):
+    __tablename__="usuarios"
+    id = Column(Integer, primary_key=True, unique=True, index=True, nullable=False)
+    username  = Column(String(255), index=True, nullable=False, unique=True)
+    email = Column(String, index=True, nullable=False, unique=True)
+    password_hash = Column(String, index=True, nullable=False)
+    role = Column(Enum(RolUsuario), nullable=False)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+
+class Tarea(Base):
+    __tablename__="tareas"
+    id = Column(Integer, primary_key=True, unique=True, index=True, nullable=False)
+    titulo = Column(String, index=True, nullable=False, unique=True)
+    descripcion = Column(String, index=True, nullable=False, unique=True)
+    estado = Column(Enum(EstadoTarea), nullable=False)
+    creado_en = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    usuario_id = Column(Integer, ForeignKey("Usuario.id"))
+    usuario = relationship("Usuario")
